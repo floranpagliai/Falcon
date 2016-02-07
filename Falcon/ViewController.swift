46d7558796ff9f7e45cb3a6c894be1fbd5745055
Floran Pagliai <floran.pagliai@gmail.com>
2016-02-07 20:35:20 +0100

    Initial Commit

---
+++ Falcon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Falcon
+//
+//  Created by Floran Pagliai on 07/02/2016.
+//  Copyright © 2016 Falcon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
