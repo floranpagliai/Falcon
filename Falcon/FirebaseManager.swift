e481fb38db0444a70eb0d4be1156490fa8b37165
Floran Pagliai <floran.pagliai@gmail.com>
2016-02-17 00:08:45 +0100

    Refactoring login Facebook

---
+++ Falcon/FirebaseManager.swift
@@ -0,0 +1,14 @@
+//
+//  FirebaseManager.swift
+//  Falcon
+//
+//  Created by Floran Pagliai on 16/02/2016.
+//  Copyright © 2016 Falcon. All rights reserved.
+//
+
+import Firebase
+
+class FirebaseManager {
+	let ref = Firebase(url: "https://falcongame.firebaseio.com")
+
+}
